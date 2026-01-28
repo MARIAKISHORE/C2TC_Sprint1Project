80f3014c15f88f7f61b5ab5c1d597353b276e008
MARIAKISHORE <156074212+MARIAKISHORE@users.noreply.github.com>
2026-01-28 21:26:29 +0530

    Delete RetailNestRepository.java

--- RetailNestRepository.java
+++
@@ -1,6 +0,0 @@
-package com.devmart.retailnestservice;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RetailNestRepository extends JpaRepository<RetailShopEntity, Long> {
-}
