f4024e0403df6ea9552233b99783ccb94e8fbc51
MARIAKISHORE <156074212+MARIAKISHORE@users.noreply.github.com>
2026-01-28 21:27:42 +0530

    Delete StaffRepository.java

--- StaffRepository.java
+++
@@ -1,8 +0,0 @@
-package com.devmart.staffsphereservice;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StaffRepository extends JpaRepository<StaffEntity, Integer> {
-}
-
