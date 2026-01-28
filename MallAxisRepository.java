d9bca6622e02426ad34439022409e64498de7f79
MARIAKISHORE <156074212+MARIAKISHORE@users.noreply.github.com>
2026-01-28 21:23:58 +0530

    Delete MallAxisRepository.java

--- MallAxisRepository.java
+++
@@ -1,6 +0,0 @@
-package com.devmart.mallaxisservice;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MallAxisRepository extends JpaRepository<MallUnitEntity, Integer> {
-}
